a3160dd810394b43ec847926a7c29ef34e119eb3
Mahesh-xr <mahesh.572xr@gmail.com>
2025-04-22 19:07:49 +0530

    add animatedCounter component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 const Button = ({text, className, id}) => {
+
+
   return (
-    <a className={`${className ?? ''} cta-wrapper`}>
+    <a className={`${className ?? ''} cta-wrapper`}
+    onClick={e=>{
+      e.preventDefault()
+      const target = document.getElementById('counter')
+      if(target && id){
+        const offset = window.innerHeight().top + window.scrollY - c+offset;;
+        window.scrollTo({
+          top,
+          behavior:"smooth"
+        })
+      }
+    }}
+    >
       <div className="cta-button group">
         <div className="bg-circle" />
         <p className="text">{text}</p>
